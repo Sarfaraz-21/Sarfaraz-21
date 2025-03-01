7a09fa53f2d2167e73866d78b4dcaaadd47cd455
Sarfaraz sk <sarfarazsk991@gmail.com>
2025-03-01 18:12:20 +0530

    storage linking problem solved

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Image;
+use Illuminate\Support\Facades\View;
 
 class ImageController extends Controller
 {
     //
     function uploadImage(Request $req)
     {
-       $path= $req->file('image')->store('img');
-         $pathArray = explode('/',$path);
+       $path= $req->file('image')->store('img','public');
+         $pathArray = \explode('/',$path);
          $imgPath = $pathArray[1];
             $image = new Image;
             $image->path = $imgPath;
--- resources/views/display-image.blade.php
+++ resources/views/display-image.blade.php
@@ -1,7 +1,12 @@
 <div>
     <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
     <h1>List of all Images</h1>
-    @foreach ($images as $image)
+    {{-- @foreach ($images as $image)
         <img src="{{ url('storage/' . $image->path) }}" alt="{{ $image->image }}" style="width: 100px; height: 100px;">
-    @endforeach
+    @endforeach --}}
+    
+    @foreach ($images as $image)
+    <img src="{{ asset('storage/img/' . $image->path) }}" alt="Image" width="200">
+@endforeach
+
 </div>
